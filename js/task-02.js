f2e84ba183fc12039a5b51853fc629665a95a6f7
KMyroslav <kukhtarukm@gmail.com>
2021-07-03 18:43:13 +0300

    Finishes task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const addNewLiEl = (parentEl, newLiArr) => {
+  const nodeToAdd = parentEl.cloneNode(true);
+  newLiArr.forEach(element => {
+    let li = document.createElement('li');
+    li.textContent = element;
+    nodeToAdd.appendChild(li);
+  });
+  parentEl.replaceWith(nodeToAdd);
+};
+
+addNewLiEl(ingredientsListEl, ingredients);
