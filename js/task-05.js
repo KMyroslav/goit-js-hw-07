6e0933da8e72b1e75449c8dd1bfe9aaf15f89d4b
KMyroslav <kukhtarukm@gmail.com>
2021-07-03 18:43:29 +0300

    Finishes task 05

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const nameInputEl = document.querySelector('#name-input');
+const nameOutputEl = document.querySelector('#name-output');
+
+nameInputEl.addEventListener('input', onChangingInput);
+
+function onChangingInput(input) {
+  nameOutputEl.textContent = input.currentTarget.value;
+}
