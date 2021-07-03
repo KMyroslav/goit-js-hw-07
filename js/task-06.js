d98034a9c71cb4df4bdc113286af0e0d209444f5
KMyroslav <kukhtarukm@gmail.com>
2021-07-03 18:43:35 +0300

    Finishes task 06

---
+++ js/task-06.js
@@ -0,0 +1,20 @@
+const validationInputEl = document.querySelector('#validation-input');
+
+validationInputEl.addEventListener('change', onInputBlur);
+
+function onInputBlur(input) {
+  if (input.currentTarget.value.length >= 1) {
+    validationInputEl.classList.add('invalid');
+  } else {
+    validationInputEl.classList.remove('invalid');
+    validationInputEl.classList.remove('valid');
+  }
+
+  if (
+    input.currentTarget.value.length ==
+    validationInputEl.getAttribute('data-length')
+  ) {
+    validationInputEl.classList.remove('invalid');
+    validationInputEl.classList.add('valid');
+  }
+}
