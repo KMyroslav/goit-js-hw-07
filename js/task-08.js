a90f0489f69e09359dad3db4cc3197057f9b23ec
KMyroslav <kukhtarukm@gmail.com>
2021-07-06 19:47:33 +0300

    Makes boxes render due to last created box

--- js/task-08.js
+++ js/task-08.js
@@ -22,20 +22,40 @@ renderButtonEl.addEventListener('click', createBoxes);
 
 clearButtonEl.addEventListener('click', destroyBoxes);
 
+// renders sizes of new boxes due to last created box
+
 function createBoxes() {
   const nodeToAdd = document.querySelector('#boxes').cloneNode(true);
   for (let i = 0; i < boxesQuantity; i += 1) {
     let box = document.createElement('div');
+    let boxesCount = document.querySelector('#boxes').childElementCount;
     box.setAttribute(
       'style',
       `background-color: ${randomColorGenerator()}; width: ${
-        30 + i * 10
-      }px; height: ${30 + i * 10}px`,
+        30 + (i + boxesCount) * 10
+      }px; height: ${30 + (i + boxesCount) * 10}px`,
     );
     nodeToAdd.appendChild(box);
   }
   document.querySelector('#boxes').replaceWith(nodeToAdd);
 }
+
+//every time creates new boxes from 30x30px
+
+// function createBoxes() {
+//   const nodeToAdd = document.querySelector('#boxes').cloneNode(true);
+//   for (let i = 0; i < boxesQuantity; i += 1) {
+//     let box = document.createElement('div');
+//     box.setAttribute(
+//       'style',
+//       `background-color: ${randomColorGenerator()}; width: ${
+//         30 + i * 10
+//       }px; height: ${30 + i * 10}px`,
+//     );
+//     nodeToAdd.appendChild(box);
+//   }
+//   document.querySelector('#boxes').replaceWith(nodeToAdd);
+// }
 
 function destroyBoxes() {
   document.querySelector('#boxes').innerHTML = '';
